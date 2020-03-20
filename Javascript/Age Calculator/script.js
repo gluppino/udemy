d8b99a0bf09deb8c9d8450c04620bbf2a18c93de
Garret Luppino <garret.luppino@gmail.com>
2020-03-20 08:07:02 -0500

    Javascript work

---
+++ Javascript/Age Calculator/script.js
@@ -0,0 +1,2 @@
+var age = prompt("what is your age?");
+
---
+++ Javascript/Conditionals/index.js
@@ -0,0 +1,9 @@
+var age = prompt("what is your age?");
+
+if (age < 0 ) {
+    console.log("Error")
+} else if (age === 21) {
+    console.log("Happy 21st Birthday")
+} else if (age => 20) {
+    console.log("Congrats")
+}
