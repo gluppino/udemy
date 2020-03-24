3408eeac18f9a7d2e9082652cc751173d194e965
Garret Luppino <garret.luppino@gmail.com>
2020-03-23 22:56:43 -0500

    functions

---
+++ Javascript/Annoyomatic/annoy.js
@@ -0,0 +1,12 @@
+var answer = prompt("Are we there yet?");
+
+while (answer !== "yes" &&  answer !== "yeah") {
+    var answer = prompt("are we there yet?"); 
+}
+
+alert("Yay We Made It!!");
+
+// if (answer === "yes") {
+//     alert("Yay, We made it!")
+    
+// }
