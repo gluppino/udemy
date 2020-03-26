a0c63cffe9c36dcc81a443011f66e3cd60dfd62d
Garret Luppino <garret.luppino@gmail.com>
2020-03-26 07:38:03 -0500

    to do list

--- Javascript/Arrays/list.js
+++ Javascript/Arrays/list.js
@@ -6,12 +6,16 @@ var input = prompt("What would you like to do?");
 while (input !== "quit") {
 
     if (input === "list") {
-        console.log(toDos);
+        console.log(**********);
+        toDos.forEach(function(todo, index) {
+        console.log(i + ": " + toDos);
+        console.log(**********);
+        });
     } else if (input === "new") {
         var newTodo = prompt("Enter new todo");
     
         toDos.push(newTodo);
-    }
+    } else if (input === "delete") {}
 
     input = prompt("What would you like to do?");
 
