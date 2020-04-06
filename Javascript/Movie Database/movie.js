6a834f258f89a409559e3ac9ffb65244306d7fe0
Garret Luppino <garret.luppino@gmail.com>
2020-04-06 08:51:15 -0500

    movie

---
+++ Javascript/Movie Database/movie.js
@@ -0,0 +1,19 @@
+var movies = [
+    {title: "Frozen",
+    hasWatched: true,
+    Rating: 4
+},
+{
+    title: Snatch,
+    hasWatched: true,
+    Rating: 3.5
+}
+];
+
+movies.forEach(movie => {
+    var result = "You have ";
+    if (movies.hasWatched) {
+        result += " watched"
+        
+    }
+});
