7c0134fd94e4bb7028fd98ead8c2c7addf849c27
Garret Luppino <garretluppino@Garrets-MacBook-Air.local>
2020-05-03 23:42:39 -0500

    color game

---
+++ DOM Manipulation/Color Game/colorgame.js
@@ -0,0 +1,61 @@
+var colors = generateRandomColors(6)
+
+var squares = document.querySelectorAll('.square')
+var pickedColor = pickColor()
+var colorDisplay = document.getElementById('colorDisplay')
+var messageDisplay = document.querySelector('#message')
+var h1 = document.querySelector('h1')
+var resetButton = document.querySelector('#reset')
+
+resetButton.addEventListener('click', function() {
+    colors = generateRandomColors(6)
+    pickedColor = pickColor()
+    colorDisplay.textContent = pickedColor
+    
+})
+
+colorDisplay.textContent = pickedColor
+
+for (let i = 0; i < squares.length; i++) {
+  // add initial color to squares
+  squares[i].style.backgroundColor = colors[i]
+
+  //   add click listeners to squares
+  squares[i].addEventListener('click', function () {
+    //   grab a color of clicked square
+    var clickedColor = this.style.background
+
+    if (clickedColor === pickedColor) {
+      messageDisplay.textContent = 'Correct'
+    } else {
+      this.style.background = '#232323'
+      messageDisplay.textContent = 'Try Again'
+    }
+  })
+}
+
+function changeColors (color) {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.background = color
+  }
+}
+
+function pickColor () {
+  var random = Math.floor(Math.random() * colors.length)
+  return colors[random]
+}
+
+function generateRandomColors (num) {
+  var arr = []
+  for (var i = 0; i < num; i++) {
+    arr.push(randomColor())
+  }
+  return arr
+}
+
+function randomColor () {
+  var r = Math.floor(Math.random() * 256)
+  var g = Math.floor(Math.random() * 256)
+  var b = Math.floor(Math.random() * 256)
+  return 'rgb(' + r + ', ' + g + ', ' + b + ')'
+}
