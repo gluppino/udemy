cd1bef4e898b16ccecbe6a421426796c0b7ae246
Garret Luppino <garret.luppino@gmail.com>
2020-05-08 23:54:37 -0500

    Click events & ToDo

---
+++ ToDo List/assests/js/todo.js
@@ -0,0 +1,20 @@
+// // check off specific todo's by clicking
+$('li').click(function () {
+    $(this).toggleClass('completed');
+
+});
+
+//     // if li is gray
+//     if ($(this).css('color') === 'rgb(128, 128, 128)') {
+//         $(this).css({
+//             color: 'black',
+//             textDecoration: 'none'
+//     }
+//     else {
+//         $(this).css({
+//           color: 'gray',
+//           textDecoration: 'line-through'
+//     }
+//         // turn it black else turn it gray
+ 
+//     })
