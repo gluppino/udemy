a71ee54395754f8bfba879d75b38e496d55353b3
Garret Luppino <garret.luppino@gmail.com>
2020-05-10 23:32:06 -0500

    Update todo.js

--- ToDo List/assests/js/todo.js
+++ ToDo List/assests/js/todo.js
@@ -4,26 +4,20 @@ $('li').click(function () {
 
 });
 
-//     // if li is gray
-//     if ($(this).css('color') === 'rgb(128, 128, 128)') {
-//         $(this).css({
-//             color: 'black',
-//             textDecoration: 'none'
-//     }
-//     else {
-//         $(this).css({
-//           color: 'gray',
-//           textDecoration: 'line-through'
-//     }
-//         // turn it black else turn it gray
- 
-//     })
-
-
-$("input[type='text']").keypress(function(event){}
-
 // Click on X to delete todo
 
-$('span').click(function (){
+$('span').click(function (e){
     alert('clicked on X');
+    $(this).parent().fadeOut(500, function(500, function() {
+        $this.remove();
+    }));
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13) {
+      var todoText = $(this).val();
+      $(this).val('');
+      $('ul').append('<li><span>X</span>' + todoText + '</li>')
+  }
 });
