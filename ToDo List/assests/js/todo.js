509f18365ac27e02b113a05e9bd9e48c971a2190
Garret Luppino <garret.luppino@gmail.com>
2020-05-10 23:42:43 -0500

    Keeping up

--- ToDo List/assests/js/todo.js
+++ ToDo List/assests/js/todo.js
@@ -1,16 +1,15 @@
 // // check off specific todo's by clicking
-$('li').click(function () {
+$('ul').on('click', 'li', function () {
     $(this).toggleClass('completed');
 
 });
 
 // Click on X to delete todo
 
-$('span').click(function (e){
-    alert('clicked on X');
-    $(this).parent().fadeOut(500, function(500, function() {
+$('ul').on('click', 'span', function (event){
+    $(this).parent().fadeOut(500, function(){
         $this.remove();
-    }));
+    });
     event.stopPropagation();
 });
 
@@ -18,6 +17,6 @@ $("input[type='text']").keypress(function(event){
   if(event.which === 13) {
       var todoText = $(this).val();
       $(this).val('');
-      $('ul').append('<li><span>X</span>' + todoText + '</li>')
+      $('ul').append(`<li><span>X</span> ${todoText}</li>`)
   }
 });
